0c944738ab38a0d192604e5d76174ae26a427588
Blair Habicht <blairh@jellycode.com>
2018-06-30 13:26:41 -0600

    Add hash to extracted CSS files
    Check each instance of ExtractTextPlugin and change the filename attribute to a contenthash hashed name. Contenthash is used for CSS files in ExtractTextPlugin.

--- src/index.js
+++ src/index.js
@@ -35,6 +35,17 @@ class VersionHash {
 
         webpackConfig.output.filename = `[name].[hash:${length}].js`
         webpackConfig.output.chunkFilename = `[name].[hash:${length}].js`
+
+        forIn(webpackConfig.plugins, (value, key) => {
+
+            if (value instanceof ExtractTextPlugin) {
+
+                value.filename = `[name].[contenthash:${length}].css`
+                webpackConfig.plugins[key] = value;
+
+            }
+            
+        })
     }
 }
 
