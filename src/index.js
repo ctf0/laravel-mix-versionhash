56167be323db2f230dfa561deff6942d77e907ca
Erutan409 <Erutan409@users.noreply.github.com>
2019-10-12 19:39:15 -0400

    Adding event listener to remove hashed mock entry mix javascript file.

--- src/index.js
+++ src/index.js
@@ -7,85 +7,134 @@ const path = require('path')
 const separator = '.'
 
 class VersionHash {
-    register(options = {}) {
-        this.options = Object.assign(
-            {
-                length: 6,
-                delimiter: separator,
-                exclude: []
-            },
-            options
-        )
 
-        const delimiter = escapeStringRegexp(this.getDelimiter())
-        const mixManifest = `${Config.publicPath}/${mix.manifest.name}`
-        const removeHashFromKeyRegex = new RegExp(`${delimiter}([a-f0-9]{${this.options.length}})\\.([^.]+)$`, 'g')
-        const removeHashFromKeyRegexWithMap = new RegExp(`${delimiter}([a-f0-9]{${this.options.length}})\\.([^.]+)\\.map$`, 'g')
+    register(options = {}) {
+
+        this.options = Object.assign({
+            length: 6,
+            delimiter: separator,
+            exclude: []
+        }, options);
+
+        const delimiter = escapeStringRegexp(this.getDelimiter());
+        const mixManifest = `${Config.publicPath}/${Mix.manifest.name}`;
+        const removeHashFromKeyRegex = new RegExp(`${delimiter}([a-f0-9]{${this.options.length}})\\.([^.]+)$`, 'g');
+        const removeHashFromKeyRegexWithMap = new RegExp(`${delimiter}([a-f0-9]{${this.options.length}})\\.([^.]+)\\.map$`, 'g');
+
+        this.registerListener();
 
         return mix.webpackConfig().then(() => {
+
             jsonfile.readFile(mixManifest, (err, obj) => {
-                let newJson = {}
+
+                let newJson = {};
 
                 forIn(obj, (value, key) => {
+
                     key = key.endsWith('.map')
                         ? key.replace(removeHashFromKeyRegexWithMap, '.$2.map')
-                        : key.replace(removeHashFromKeyRegex, '.$2')
+                        : key.replace(removeHashFromKeyRegex, '.$2');
 
-                    newJson[key] = value
-                })
+                    newJson[key] = value;
+
+                });
 
                 jsonfile.writeFile(mixManifest, newJson, {spaces: 2}, (err) => {
-                    if (err) console.error(err)
-                })
-            })
-        })
+                    
+                    if (err) {
+                        console.error(err);
+                    }
+
+                });
+
+            });
+
+        });
+
     }
 
     dependencies() {
-        return ['jsonfile', 'escape-string-regexp', 'path']
+        return ['jsonfile', 'escape-string-regexp', 'path'];
     }
 
     webpackConfig(webpackConfig) {
-        const length = this.options.length
-        const delimiter = this.getDelimiter()
+
+        const length = this.options.length;
+        const delimiter = this.getDelimiter();
 
         // js
-        let chunkhash = `[name]${delimiter}[chunkhash:${length}].js`
+        let chunkhash = `[name]${delimiter}[chunkhash:${length}].js`;
 
-        webpackConfig.output.filename = chunkhash
+        webpackConfig.output.filename = chunkhash;
 
-        let usesExtract = webpackConfig.optimization && webpackConfig.optimization.runtimeChunk
+        let usesExtract = webpackConfig.optimization && webpackConfig.optimization.runtimeChunk;
+
         if (webpackConfig.output.chunkFilename && !usesExtract) {
+
             // merge chunkFilename paths
-            let directory = path.dirname(webpackConfig.output.chunkFilename)
-            webpackConfig.output.chunkFilename = `${directory}/${chunkhash}`
+            let directory = path.dirname(webpackConfig.output.chunkFilename);
+            webpackConfig.output.chunkFilename = `${directory}/${chunkhash}`;
+
         } else {
-            webpackConfig.output.chunkFilename = chunkhash
+            webpackConfig.output.chunkFilename = chunkhash;
         }
 
         // css
         let contenthash = `[hash:${length}].css`
 
         forIn(webpackConfig.plugins, (value, key) => {
+
             if (value instanceof ExtractTextPlugin && !value.filename.includes(contenthash)) {
 
-                let csspath = value.filename.substring(0, value.filename.lastIndexOf('.'))
-                let filename = `${csspath}${delimiter}${contenthash}`
+                let csspath = value.filename.substring(0, value.filename.lastIndexOf('.'));
+                let filename = `${csspath}${delimiter}${contenthash}`;
 
                 if (value.filename != filename) {
-                    value.filename = filename
+                    value.filename = filename;
                 }
+
             }
-        })
+
+        });
+
     }
 
     getDelimiter() {
-        return this.options.delimiter.replace(/[^\.|\-|_]/g, '') || separator
+        return this.options.delimiter.replace(/[^\.|\-|_]/g, '') || separator;
     }
 
     exclude(key) {
-        return this.options.exclude.some((e) => e == key)
+        return this.options.exclude.some((e) => e == key);
     }
+
+    registerListener() {
+
+        const mixFileRegex = new RegExp(`^mix${this.getDelimiter()}.{${this.options.length}}\.js$`, 'i');
+
+        Mix.listen('build', stats => {
+
+            if (!Mix.bundlingJavaScript) {
+
+                let temporaryOutputFile = stats
+                    .toJson()
+                    .assets.find(asset => mixFileRegex.test(asset.name));
+        
+                if (temporaryOutputFile) {
+        
+                    delete stats.compilation.assets[temporaryOutputFile.name];
+        
+                    File.find(
+                        path.resolve(Config.publicPath, temporaryOutputFile.name)
+                    ).delete();
+        
+                }
+
+            }
+
+        });
+
+    }
+
 }
 
-mix.extend('versionHash', new VersionHash())
+mix.extend('versionHash', new VersionHash());
