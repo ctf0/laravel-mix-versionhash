16c0a562ad0c3afdb58e13b143f9687571fb077d
Erutan409 <Erutan409@users.noreply.github.com>
2019-11-02 12:03:47 -0400

    Adding functionality to detect when combining file(s). * Uses built-in Laravel Mix hashing functionality * Implements a webpack plugin that will update the stats asset listing for combined file(s)

--- src/index.js
+++ src/index.js
@@ -1,88 +1,215 @@
-const mix = require('laravel-mix')
-const forIn = require('lodash/forIn')
-const escapeStringRegexp = require('escape-string-regexp')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const path = require('path')
-const separator = '.'
+const mix = require('laravel-mix');
+const File = require('laravel-mix/src/File');
+const proxyMethod = require('proxy-method');
+const ConcatenateFilesTask = require('laravel-mix/src/tasks/ConcatenateFilesTask');
+const forIn = require('lodash/forIn');
+const escapeStringRegexp = require('escape-string-regexp');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+const separator = '.';
 
+/**
+ * Version Hash for Laravel Mix.
+ *
+ * @see https://laravel-mix.com/
+ */
 class VersionHash {
 
+    /**
+     * Constructor.
+     */
+    constructor() {
+
+        // hash the generated assets once build is complete
+        this.registerHashAssets();
+
+        // look for instances of combining file(s)
+        this.hashForCombine();
+
+    }
+
+    /**
+     * Dependencies for plugin.
+     *
+     * @return {String[]}
+     */
     dependencies() {
         return ['jsonfile', 'escape-string-regexp', 'path']
     }
-    
+
+    /**
+     * Plugin functionality.
+     *
+     * @param {{length: {Number}, delimiter: {String}, exclude: {String[]}}} options
+     */
     register(options = {}) {
+
         this.options = Object.assign({
             length: 6,
             delimiter: separator,
             exclude: []
-        }, options)
+        }, options);
 
-        // hash the generated assets once build is complete
-        this.registerHashAssets()
     }
 
+    /**
+     * Apply configuration to webpack configuration.
+     *
+     * @param {Object} webpackConfig
+     */
     webpackConfig(webpackConfig) {
-        const length = this.options.length
-        const delimiter = this.getDelimiter()
+
+        const length = this.options.length;
+        const delimiter = this.getDelimiter();
 
         // js
-        let chunkhash = `[name]${delimiter}[chunkhash:${length}].js`
-        let usesExtract = webpackConfig.optimization && webpackConfig.optimization.runtimeChunk
-        webpackConfig.output.filename = chunkhash
+        let chunkhash = `[name]${delimiter}[chunkhash:${length}].js`;
+        let usesExtract = webpackConfig.optimization && webpackConfig.optimization.runtimeChunk;
+        webpackConfig.output.filename = chunkhash;
 
         if (webpackConfig.output.chunkFilename && !usesExtract) {
+
             // merge chunkFilename paths
-            let directory = path.dirname(webpackConfig.output.chunkFilename)
-            webpackConfig.output.chunkFilename = `${directory}/${chunkhash}`
+            let directory = path.dirname(webpackConfig.output.chunkFilename);
+            webpackConfig.output.chunkFilename = `${directory}/${chunkhash}`;
+
         } else {
-            webpackConfig.output.chunkFilename = chunkhash
+            webpackConfig.output.chunkFilename = chunkhash;
         }
 
         // css
-        let contenthash = `[hash:${length}].css`
+        let contenthash = `[hash:${length}].css`;
 
-        forIn(webpackConfig.plugins, (value, key) => {
+        forIn(webpackConfig.plugins, value => {
+
             if (value instanceof ExtractTextPlugin && !value.filename.includes(contenthash)) {
-                let csspath = value.filename.substring(0, value.filename.lastIndexOf('.'))
-                let filename = `${csspath}${delimiter}${contenthash}`
+
+                let csspath = value.filename.substring(0, value.filename.lastIndexOf('.'));
+                let filename = `${csspath}${delimiter}${contenthash}`;
 
                 if (value.filename != filename) {
-                    value.filename = filename
+                    value.filename = filename;
                 }
+
             }
-        })
+
+        });
+
     }
 
+    /**
+     * Get configured delimiter with appropriate filtering.
+     *
+     * @return {String}
+     */
     getDelimiter() {
-        return this.options.delimiter.replace(/[^\.|\-|_]/g, '') || separator
+        return this.options.delimiter.replace(/[^.|\-_]/g, '') || separator;
     }
 
+    /**
+     * TODO vet whether or not this needs to exist...?
+     */
     exclude(key) {
-        return this.options.exclude.some((e) => e == key)
+        return this.options.exclude.some(e => e == key);
     }
 
+    /**
+     * Add listener to account for hashing in filename(s) persisted to manifest.
+     *
+     * @return {this}
+     */
     registerHashAssets() {
-        const delimiter = escapeStringRegexp(this.getDelimiter())
-        const removeHashFromKeyRegex = new RegExp(`${delimiter}([a-f0-9]{${this.options.length}})\\.([^.]+)$`, 'g')
-        const removeHashFromKeyRegexWithMap = new RegExp(`${delimiter}([a-f0-9]{${this.options.length}})\\.([^.]+)\\.map$`, 'g')
 
         Mix.listen('build', () => {
-            const file = File.find(`${Config.publicPath}/${Mix.manifest.name}`)
-            let newJson = {}
+
+            const delimiter = escapeStringRegexp(this.getDelimiter());
+            const removeHashFromKeyRegex = new RegExp(`${delimiter}([a-f0-9]{${this.options.length}})\\.([^.]+)$`, 'g');
+            const removeHashFromKeyRegexWithMap = new RegExp(`${delimiter}([a-f0-9]{${this.options.length}})\\.([^.]+)\\.map$`, 'g');
+
+            const file = File.find(`${Config.publicPath}/${Mix.manifest.name}`);
+            let newJson = {};
 
             forIn(JSON.parse(file.read()), (value, key) => {
-                key = key.endsWith('.map')
-                    ? key.replace(removeHashFromKeyRegexWithMap, '.$2.map')
-                    : key.replace(removeHashFromKeyRegex, '.$2')
 
-                newJson[key] = value
-            })
+                if (key.endsWith('.map')) {
+                    key = key.replace(removeHashFromKeyRegexWithMap, '.$2.map');
+                } else {
+                    key = key.replace(removeHashFromKeyRegex, '.$2');
+                }
 
-            file.write(newJson)
-        })
+                newJson[key] = value;
+
+            });
+
+            file.write(newJson);
+
+        });
+
+        return this;
+
+    }
+
+    /**
+     * Intercept functionality that generates combined asset(s).
+     *
+     * @return {this}
+     */
+    hashForCombine() {
+
+        const combinedFiles = {};
+
+        // hook into Mix's task collection to update file name hashes
+        proxyMethod.before(Mix, 'addTask', task => {
+
+            if (task instanceof ConcatenateFilesTask) {
+
+                proxyMethod.after(task, 'merge', () => {
+
+                    const file = task.assets.pop();
+                    const hash = `${this.getDelimiter()}${file.version().substr(0, this.options.length)}`;
+                    const hashed = file.rename(`${file.nameWithoutExtension()}${hash}${file.extension()}`);
+                    task.assets.push(hashed);
+
+                    combinedFiles[hashed.pathFromPublic()] = true;
+
+                });
+
+            }
+
+        });
+
+        // update backslashes to forward slashes for consistency
+        Mix.listen('loading-plugins', plugins => {
+
+            plugins.push(new class {
+
+                apply(compiler) {
+
+                    compiler.plugin('done', stats => {
+
+                        forIn(stats.compilation.assets, (asset, path) => {
+
+                            if (combinedFiles[path]) {
+
+                                delete stats.compilation.assets[path];
+                                stats.compilation.assets[path.replace(/\\/g, '/')] = asset;
+
+                            }
+
+                        })
+
+                    });
+
+                }
+
+            });
+
+        });
+
+        return this;
+
     }
 
 }
 
-mix.extend('versionHash', new VersionHash())
+mix.extend('versionHash', new VersionHash());
